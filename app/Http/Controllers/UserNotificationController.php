583e1b8ca8ff81cedfe820fe2cc443aea796c5ec
Orion Pobursky <orion@ldraw.org>
2023-03-27 19:04:40 +0000

    Fix track toggle

--- app/Http/Controllers/UserNotificationController.php
+++ app/Http/Controllers/UserNotificationController.php
@@ -2,15 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use App\Models\Part;
 
 class UserNotificationController extends Controller
 {
-  public function store(User $user, Part $part, Request $request) {
-    if (!is_null($request->user()) && $request->user()->id == $user->id) {
-      $user->togglePartNotification($part);
+  public function store(Part $part) {
+    if (Auth::check()) {
+      Auth::user()->togglePartNotification($part);
     }
     return back();
   }
