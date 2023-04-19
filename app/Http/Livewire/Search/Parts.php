f210727c6491c2e2b0de308c390c0144fa234e69
Orion Pobursky <orion@ldraw.org>
2023-04-19 04:44:36 +0000

    Livewire part search

---
+++ app/Http/Livewire/Search/Parts.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Livewire\Search;
+
+use Livewire\Component;
+use App\Models\Part;
+
+class Parts extends Component
+{
+    public $search;
+    public $scope = 'header';
+    public $uparts;
+    public $oparts;
+
+    protected $queryString= [
+        'search' => ['except' => '', 'as' => 's'],
+        'scope' => ['except' => 'header']
+    ];
+
+    public function mount()
+    {
+        $this->search();
+    }
+
+    public function render()
+    {
+        return view('livewire.search.parts');
+    }
+
+    public function search() {
+        $this->scope = in_array($this->scope, ['filename', 'description', 'header', 'file'], true) ? $this->scope : 'header';
+        if (!empty($this->search)) {
+            $parts = Part::searchPart($this->search, $this->scope)->orderBy('filename')->get();
+            $this->uparts = $parts->where('release.short', 'unof');
+            $this->oparts = $parts->where('release.short', '<>', 'unof');    
+        }
+    }
+}
---
+++ resources/views/livewire/search/parts.blade.php
@@ -0,0 +1,24 @@
+<div>
+  <form class="ui form" wire:submit.prevent="search">
+    <div class="field">
+      <label>Search terms:</label>
+      <div class="ui action input">
+        <input type="text" wire:model="search">
+        <button class="ui button">Go</button>
+      </div>
+    </div> 
+    <div class="four wide field">
+      <label>Search Scope:</label>
+      <select wire:model="scope">
+        <option value="filename" @selected($scope == 'filename')>Filename only</option>
+        <option value="description" @selected($scope == 'description')>Filename and description</option>
+        <option value="header" @selected($scope == 'header')>Filename and file header</option>
+        <option value="file" @selected($scope == 'file')>Filename and entire file</option>
+      </select>
+    </div>  
+  </form>
+  @if(!is_null($uparts) || !is_null($oparts))
+      <x-part.table title="Matched {{$uparts->count() ?? 0}} Unofficial Parts" :parts="$uparts" none=""/>
+      <x-part.table title="Matched {{$oparts->count() ?? 0}} Official Parts" :parts="$oparts" none=""/>
+  @endif  
+</div>
