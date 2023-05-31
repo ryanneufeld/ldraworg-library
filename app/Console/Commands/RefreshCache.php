0c23940831f147d574d395493becac5ef53b475f
Orion Pobursky <orion@ldraw.org>
2023-05-31 00:29:16 +0000

    Command to refresh the cache after update

---
+++ app/Console/Commands/RefreshCache.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class RefreshCache extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:refresh';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Refresh app cache after code update';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->call('cache:clear');
+        $this->call('view:clear');
+        $this->call('route:clear');
+        $this->call('config:clear');
+        $this->call('view:cache');
+        $this->call('route:cache');
+        $this->call('config:cache');
+     }
+}
