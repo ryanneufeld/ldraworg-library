1501bef8cbd283ab6ce3dfdfc1401d9deb65e4d8
Orion Pobursky <orion@ldraw.org>
2023-06-26 05:07:35 +0000

    Remove drag and drop editing from review summaries

--- app/Http/Livewire/ReviewSummaryEdit.php
+++ app/Http/Livewire/ReviewSummaryEdit.php
@@ -9,33 +9,7 @@ use App\Models\ReviewSummaryItem;
 class ReviewSummaryEdit extends Component
 {
     public ReviewSummary $summary;
-    public $newPartId;
-    public $newHeading;
     public $manualEntry;
-
-    public function addItem() {
-        if(!empty($this->newPartId)) {
-            $order = $this->summary->items()->orderBy('order', 'DESC')->first()->order ?? 0;
-            ReviewSummaryItem::create([
-                'part_id' => $this->newPartId,
-                'order' => $order + 1,
-                'review_summary_id' => $this->summary->id
-            ]);
-            $this->summary->refresh();
-            $this->newPartId = null;    
-        }
-    }
-
-    public function addHeading() {
-        $order = $this->summary->items()->orderBy('order', 'DESC')->first()->order ?? 0;
-        ReviewSummaryItem::create([
-            'heading' => empty($this->newHeading) ? '' :  $this->newHeading,
-            'order' => $order + 1,
-            'review_summary_id' => $this->summary->id
-        ]);
-        $this->summary->refresh();
-        $this->newHeading = null;
-    }
 
     public function processManualEntry() {
         if(!empty($this->manualEntry)) {
@@ -67,25 +41,6 @@ class ReviewSummaryEdit extends Component
             }
             $this->summary->refresh();
         }        
-    }
-
-    public function removeItem(ReviewSummaryItem $item) {
-        $item->delete();
-        //Rerack the order
-        foreach($this->summary->items()->orderBy('order')->get() as $i => $item) {
-            $item->order = $i + 1;
-            $item->save();
-        }
-        $this->summary->refresh();
-    }
-
-    public function updateItemOrder($reorderedItems) {
-        foreach($reorderedItems as $itm) {
-            $item = ReviewSummaryItem::find($itm['value']);
-            $item->order = $itm['order'];
-            $item->save();
-        }
-        $this->summary->refresh();
     }
 
     public function render()
--- resources/views/livewire/review-summary-edit.blade.php
+++ resources/views/livewire/review-summary-edit.blade.php
@@ -1,46 +1,16 @@
 <div>
-    <div class="ui top attached tabular summarymenu menu">
-        <a class="item active" data-tab="normal">Normal Edit</a>
-        <a class="item" data-tab="manual">Manual Edit</a>
-    </div>
-    <div class="ui bottom attached tab segment active" data-tab="normal">
-        <div class="ui relaxed divided list" wire:sortable="updateItemOrder">
-            @foreach ($summary->items as $i => $item)
-                <div class="item" wire:sortable.item="{{ $item->id }}" wire:key="item-{{ $item->id }}">
-                    <i wire:sortable.handle class="large github middle aligned icon"></i>
-                    <div class="content">
-                        @if(is_null($item->heading))
-                            {{$item->part->filename}} - {{$item->part->description}}
-                        @else
-                            Heading: {{$item->heading == '' ? 'Blank' : $item->heading}}
-                        @endif
-                        <button class="ui button" wire:click="removeItem({{ $item->id }})">Remove</button>
-                    </div>        
-                </div>
-            @endforeach
-        </div>
-        <form class="ui form" id="part" wire:submit.prevent="addItem">
-            <x-form.select-unofficial-part name="newPartId" wire:model="newPartId" label="Add Part" withDescription actionInput formId="part" buttonLabel="Add" selected="{{$newPartId}}"/>
-        </form>    
-        <form class="ui form" id="heading" wire:submit.prevent="addHeading">
-            <div class="ui field">
-                <label>Add Heading</label>
-                <div class="ui action input">
-                    <input type="text" wire:model="newHeading" placeholder="New Heading">
-                    <button form="heading" class="ui button">Add</button>
-                </div>
-            </div>    
-        </form>
-    </div>
-    <div class="ui bottom attached tab segment" data-tab="manual">        
-        <form class="ui form" id="manual" wire:submit.prevent="processManualEntry">
-            <div class="ui field">
-                <label>Manual Edit</label>
-                <textarea rows="30" wire:model.defer="manualEntry">{{$summary->toString()}}</textarea>
-            </div>    
-            <div class="ui field">
-                <button form="manual" class="ui button">Edit</button>
+    <form class="ui form" id="manual" wire:submit.prevent="processManualEntry">
+        <div class="ui field">
+            <label>Parts List</label>
+            <div class="ui blue segment">
+                Each line should be the folder and part number of an unofficial part<br>
+                Only unofficial parts should be listed, official parts will be automatically removed<br>
+                Use "/" for a dividing line, optional text may be added for a heading.
             </div>
-        </form>
-    </div>    
+            <textarea rows="30" wire:model.defer="manualEntry">{{$summary->toString()}}</textarea>
+        </div>    
+        <div class="ui field">
+            <button form="manual" class="ui button">Edit</button>
+        </div>
+    </form>
 </div>
