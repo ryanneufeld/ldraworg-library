54c149a238d56efeedd9fb032ef0b91edea852a3
Orion Pobursky <orion@ldraw.org>
2023-08-23 03:42:45 +0000

    Fix submitted parts bug

--- app/Http/Controllers/UserDashboardController.php
+++ app/Http/Controllers/UserDashboardController.php
@@ -15,7 +15,12 @@ class UserDashboardController extends Controller
     public function __invoke() {
         $user = Auth::user();
         $partSort = [['vote_sort','asc'],['type.folder', 'asc'],['description', 'asc']];
-        $submits = Part::unofficial()->userSubmits($user)->get()->sortBy($partSort);
+        $submits = Part::unofficial()
+            ->whereHas('events', function($q){
+                $q->whereRelation('part_event_type', 'slug', 'submit')->where('user_id', Auth::user()->id);
+            })
+            ->get()
+            ->sortBy($partSort);
         $submitIds = $submits->pluck('id')->all();
         $votes = Vote::with(['part'])->where('user_id', $user->id)->take(500)->get()->sortBy([['vote_type_code','asc'],['part.type.folder', 'asc'],['part.description', 'asc']]);
         $tracked = $user->notification_parts()->take(500)->get()->sortBy($partSort);
