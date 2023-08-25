0a74a39f464bc56809e73ea0310d69d2456cc21a
Orion Pobursky <orion@ldraw.org>
2023-08-25 00:42:34 +0000

    Fix bug with db call

--- app/Http/Controllers/Part/NonAdminReleaseController.php
+++ app/Http/Controllers/Part/NonAdminReleaseController.php
@@ -12,11 +12,16 @@ class NonAdminReleaseController extends Controller
     ) {}
 
     public function __invoke() {
-        $parts = Part::with(['parents', 'subparts'])->unofficial()->where('vote_sort', 1)->orderBy('part_type_id')->orderBy('filename')->get();
-        $parts = $parts->reject(function (Part $part) {
-            $check = $this->checker->checkCanRelease($part);
-            return !$check['can_release'];
-        });
+        $parts = Part::with('descendants', 'ancestors')
+            ->unofficial()
+            ->where('vote_sort', 1)
+            ->orderBy('part_type_id')
+            ->orderBy('filename')
+            ->get()
+            ->reject(function (Part $part) {
+                $check = $this->checker->checkCanRelease($part);
+                return !$check['can_release'];
+            });
         return view('part.nextrelease', compact('parts'));
     }
 }
