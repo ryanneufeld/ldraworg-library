1254035efc2c901b8a0f8e2598a92c2c06ce7b3c
Orion Pobursky <orion@ldraw.org>
2023-08-25 01:29:43 +0000

    Minor fixes to dashboard

--- app/Http/Controllers/AdminDashboardController.php
+++ app/Http/Controllers/AdminDashboardController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\LDraw\Parse\ParsedPart;
 use App\Models\Part;
 
 class AdminDashboardController extends Controller
 {
-    public function __construct() {
+    public function __construct(
+        protected \App\LDraw\Check\PartChecker $checker
+    ) {
         $this->middleware('auth');
     }
     
@@ -18,9 +21,15 @@ class AdminDashboardController extends Controller
             ->orderBy('part_type_id')
             ->oldest()
             ->get();
-        $prims = Part::official()->notLicenseName('CC_BY_4')->whereHas('type', function ($q) {
-            $q->whereIn('type', ['Primitive', '48_Primitive', '8_Primitive']);
-        })->withCount('parents')->orderBy('parents_count', 'desc')->take(100)->get();
+        $prims = Part::official()
+            ->notLicenseName('CC_BY_4')
+            ->whereHas('type', function ($q) {
+                $q->whereIn('type', ['Primitive', '48_Primitive', '8_Primitive']);
+            })
+            ->withCount('parents')
+            ->orderBy('parents_count', 'desc')
+            ->take(100)
+            ->get();
         return view('admin.dashboard', compact('delete_flags', 'manual_hold_flags', 'adminreadyparts', 'prims'));
     }
   
