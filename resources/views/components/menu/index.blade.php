633386d8f3af207bf5f65bad949b10c93a919360
Orion Pobursky <orion@ldraw.org>
2024-01-06 14:37:54 +0000

    menu updates, update laravel-permission

--- resources/views/components/menu/index.blade.php
+++ resources/views/components/menu/index.blade.php
@@ -1,12 +1,12 @@
-@props(['menuname', 'submenu' => false, 'nested' => false])
-@empty($submenu)
+@props(['menuname', 'level' => 0])
+@if($level == 0)
     <ul class="flex flex-row divide-x bg-white border rounded-md w-max">
-@else        
+@else
     <ul 
         @class([
             'flex flex-col bg-white absolute divide-y border rounded-md w-max z-10',
-            'mt-2 left-0 end-0' => !$nested,
-            'left-3/4 end-0' => $nested])
+            'mt-2 left-0 end-0' => $level == 1,
+            'left-3/4 end-0' => $level > 1])
         x-show="{{$menuname}}" 
         x-transition:enter="transition ease-out duration-100" 
         x-transition:enter-start="transform opacity-0"
--- resources/views/components/menu/item.blade.php
+++ resources/views/components/menu/item.blade.php
@@ -1,21 +1,19 @@
-@props(['label', 'link' => null, 'dropdown' => null, 'toplevel' => null])
+@aware(['level'])
+@props(['label', 'link' => null, 'dropdown' => null])
 @isset($dropdown)
     @php 
         $mname = strtolower(preg_replace('/[^A-Za-z0-9_]/', '', $label)) 
     @endphp
     <li x-data="{ {{$mname}} : false }" @@mouseover="{{$mname}} = true" @@mouseover.away="{{$mname}} = false" class="p-2 hover:bg-gray-300 relative">
         {{$label}}
-        @isset($toplevel)
+        @if($level == 0)
             <x-fas-caret-down class="inline size-4" />
-            <x-menu menuname="{{$mname}}" submenu >
-                {{$slot}}
-            </x-menu>
         @else
             <x-fas-caret-right class="inline size-4" />
-            <x-menu menuname="{{$mname}}" submenu nested>
-                {{$slot}}
-            </x-menu>
-        @endisset    
+        @endif
+        <x-menu menuname="{{$mname}}" level="{{($level ?? 0) + 1}}">
+            {{$slot}}
+        </x-menu>
     </li>
 @else
     <li class="p-2 hover:bg-gray-300">
