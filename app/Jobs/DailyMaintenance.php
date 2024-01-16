59b34554adb5647070e6ab59fb2ae2975f8b5f5e
Orion Pobursky <orion@ldraw.org>
2024-01-16 02:55:18 +0000

    Remove orphan images

--- app/Jobs/DailyMaintenance.php
+++ app/Jobs/DailyMaintenance.php
@@ -2,6 +2,7 @@
 
 namespace App\Jobs;
 
+use App\LDraw\PartManager;
 use App\Models\Part;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
@@ -28,6 +29,47 @@ class DailyMaintenance implements ShouldQueue
      */
     public function handle(): void
     {
-        Part::lazy()->each(fn (Part $p) => app(\App\LDraw\PartManager::class)->loadSubpartsFromBody($p));
+        // Reload all subparts
+        Part::lazy()->each(fn (Part $p) => app(PartManager::class)->loadSubpartsFromBody($p));
+
+        // Recount all votes
+        Part::unofficial()->lazy()->each(fn (Part $p) => $p->updateVoteData());
+
+        // Find and remove orphan images
+        $images = Storage::disk('images')->allFiles('library/unofficial');
+        $files = collect($images)
+            ->map( function(string $file): string {
+                $file = str_replace('_thumb.png', '.png', $file);
+                if (strpos($file, 'textures/') !== false) {
+                    return str_replace('library/unofficial/', '', $file);
+                } else {
+                    return str_replace(['library/unofficial/', '.png'], ['', '.dat'], $file);
+                }                
+            })
+            ->unique()
+            ->all();
+        $in_use_files = Part::unofficial()
+            ->whereIn('filename', $files)
+            ->pluck('filename')
+            ->map( 
+                fn (string $filename): string =>
+                    strpos($filename, 'textures/') !== false ? "library/unofficial/{$filename}" : str_replace('.dat', '.png', "library/unofficial/{$filename}")
+            );
+        foreach ($images as $image) {
+            if (!$in_use_files->contains($image) && !$in_use_files->contains(str_replace('_thumb.png', '.png', $image))) {
+                dd($image);
+                Storage::disk('images')->delete($image);
+            }
+        }
+
+        // Find unofficial parts without images and regenerate them
+        Part::unofficial()->lazy()->each(function (Part $p) {
+            $image = str_replace('dat', '.png', "library/unofficial/{$p->filename}");
+            $thumb = str_replace('.png', '_thumb.png', $image);
+            if (!Storage::disk('images')->exists($image) || !Storage::disk('images')->exists($thumb)) {
+                app(PartManager::class)->updatePartImage($p);
+            }
+        });
+
     }
 }
