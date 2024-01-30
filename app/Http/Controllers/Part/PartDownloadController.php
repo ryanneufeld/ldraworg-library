026cfcbe38a5538b41cb9cf40c2d9ac753eddf72
Orion Pobursky <orion@ldraw.org>
2024-01-30 00:04:39 +0000

    Add If-Modified-Since reply for downloads

--- app/Http/Controllers/Part/PartDownloadController.php
+++ app/Http/Controllers/Part/PartDownloadController.php
@@ -15,14 +15,18 @@ class PartDownloadController extends Controller
      */
 
     public function __invoke(Part $part) {
-        return response()->streamDownload(function() use ($part) { 
-            echo $part->get(); 
-        }, 
-	basename($part->filename), 
-	[
-	    'Content-Type' => $part->isTexmap() ? 'image/png' : 'text/plain',
-            'Last-Modified' => $part->updated_at
-
-        ]);
+        if ($part->updated_at->format('U') < date_format(date_create(request()->header('If-Modified-Since', date('r', 0))), 'U')
+        ) {
+            return response(null ,304)->header('Last-Modified', $part->updated_at->format('r'));
+        } else {
+            return response()->streamDownload(function() use ($part) { 
+                echo $part->get(); 
+            }, 
+            basename($part->filename), 
+            [
+                'Content-Type' => $part->isTexmap() ? 'image/png' : 'text/plain',
+                'Last-Modified' => $part->updated_at->format('r')
+            ]);
+        }
     }
 }
