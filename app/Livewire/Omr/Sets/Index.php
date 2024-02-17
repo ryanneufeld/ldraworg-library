93417d79c3841ddb615f0b6c8ba4e0d658820d6d
Orion Pobursky <orion@ldraw.org>
2024-02-17 22:59:17 +0000

    filament omr index

--- app/Livewire/Omr/Sets/Index.php
+++ app/Livewire/Omr/Sets/Index.php
@@ -2,19 +2,72 @@
 
 namespace App\Livewire\Omr\Sets;
 
+use App\Models\Omr\OmrModel;
+use App\Models\Omr\Set;
+use App\Models\User;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\ViewColumn;
+use Filament\Tables\Concerns\InteractsWithTable;
+use Filament\Tables\Contracts\HasTable;
+use Filament\Tables\Enums\FiltersLayout;
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Tables\Table as Table;
 use Livewire\Component;
-use Livewire\WithPagination;
+use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Route;
 
-class Index extends Component
+class Index extends Component implements HasForms, HasTable
 {
-    use WithPagination;
-    
+    use InteractsWithForms;
+    use InteractsWithTable;
+
+    public bool $unofficial = false;
+
+    public function mount()
+    {
+        $this->unofficial = Route::currentRouteName() == 'official.index' ? false : true;
+    }
+
+    public function table(Table $table): Table
+    {
+
+        return $table
+            ->query(OmrModel::query())
+            ->emptyState(view('tables.empty', ['none' => 'None']))
+            ->columns([
+                ImageColumn::make('set.rb_url')
+                    ->extraImgAttributes(['class' => 'object-scale-down']),
+                TextColumn::make('set.number')
+                    ->label('Set Number')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('set.name')
+                    ->label('Set Name')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('name')
+                    ->state(fn(OmrModel $m) => $m->alt_model_name ?? 'Main Model'),
+                TextColumn::make('user')
+                    ->state(fn(OmrModel $m) => $m->user->authorString())
+                    ->sortable(),
+            ])
+            ->filters([
+            ], layout: FiltersLayout::AboveContent)
+                ->persistFiltersInSession()
+            ->recordUrl(
+                fn (OmrModel $m): string => 
+                    route('omr.sets.show', $m->set)
+            )
+            ->striped()
+            ->paginated([50, 100, 250, 500])
+            ->defaultPaginationPageOption(50);
+    }
+
     public function render()
     {
-        $this->dispatch('jquery');
-        return view('livewire.omr.sets.index', [
-            'sets' => \App\Models\Omr\Set::has('models')->orderBy('number')->paginate(100),
-        ]);
-        return view('livewire.omr.sets.index');
+        return view('livewire.tables.basic-table')->layout('components.layout.omr');
     }
 }
--- resources/views/omr/index.blade.php
+++
@@ -1,9 +0,0 @@
-<x-layout.omr>
-    <x-slot:title>
-        LDraw.org Official Model Repository - Model List
-    </x-slot>
-    <x-slot:breadcrumbs>
-        <x-breadcrumb-item class="active" item="Model List" />
-    </x-slot>
-    <livewire:omr.sets.index />  
-  </x-layout.omr>
