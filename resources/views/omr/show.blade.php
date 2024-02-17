fa69889a3e077df9913cdc4697a9d4310a22d460
Orion Pobursky <orion@ldraw.org>
2024-02-17 22:23:30 +0000

    tailwind omr show

--- resources/views/omr/show.blade.php
+++ resources/views/omr/show.blade.php
@@ -5,139 +5,41 @@
     @endpush
       <x-slot:breadcrumbs>
       <x-breadcrumb-item class="active" item="Set Detail" />
-    </x-slot>    
-    <h2 class="ui block header">{{$set->number}} - {{$set->name}}</h2>
-    <div class="ui grid">
-        <div class="eleven wide column">
-            <img class="ui image" src="{{$set->rb_url}}">
-{{--
-            <div id="model-container">
-                <canvas id="model-canvas"></canvas>
+    </x-slot>
+    <div class="flex flex-col space-y-2">    
+        <div class="rounded border text-xl font-bold bg-gray-200 p-2">{{$set->number}} - {{$set->name}}</div>
+        <div class="grid grid-cols-12 gap-2">
+            <div class="col-span-8">
+                <img class="object-scale-down" src="{{$set->rb_url}}">
             </div>
---}}
-        </div>
-        <div class="five wide column">
-            <h3 class="ui block header">Models</h3>
-            @foreach($set->models->sortBy('alt_model') as $model)
-                <table class="ui celled striped stackable table">
-                    <thead>
-                        <tr>
-                            <th colspan="3">
-                                {{$model->alt_model_name ?? 'Main Model'}}
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>
-                                Author
-                            </td>
-                            <td colspan="2">
-                                {{$model->user->authorString()}}
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                Missing Parts
-                            </td>
-                            <td>
-                                Missing Patterns
-                            </td>
-                             <td>
-                                Missing Stickers
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
+            <div class="flex flex-col col-span-4 space-y-2">
+                <div class="rounded border text-lg font-bold bg-gray-200 p-2">Models</div>
+                @foreach($set->models->sortBy('alt_model') as $model)
+                    <div class="flex flex-col rounded border">
+                        <div class="font-bold bg-gray-200 p-2">
+                            {{$model->alt_model_name ?? 'Main Model'}}
+                        </div>
+                        <div class="p-2">
+                            <span class="font-bold pr-2">Author:</span>{{$model->user->authorString()}}
+                        </div>
+                        <div class="grid grid-cols-3 p-2">
+                            <div>
+                                <span class="font-bold">Missing Parts</span><br>
                                 {{$model->missing_parts ? 'Yes' : 'No'}}
-                            </td>
-                            <td>
+                            </div>    
+                            <div>
+                                <span class="font-bold">Missing Patterns</span><br>
                                 {{$model->missing_patterns ? 'Yes' : 'No'}}
-                            </td>
-                            <td>
+                            </div>    
+                            <div>
+                                <span class="font-bold">Missing Stickers</span><br>
                                 {{$model->missing_stickers ? 'Yes' : 'No'}}
-                            </td>
-                        </tr>
-                        <tr>
-                            <td colspan="3">
-                                <a class="ui primary button" href="{{asset('library/omr/' . $model->filename())}}">Download</a>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            @endforeach    
+                            </div>    
+                        </div>
+                        <a class="rounded-lg border bg-blue-500 font-bold px-4 py-2 text-white m-2 w-max" href="{{asset('library/omr/' . $model->filename())}}">Download</a>
+                    </div>
+                @endforeach    
+            </div>
         </div>
     </div>
-{{--    
-    @push('scripts')
-        <script src="/assets/ldbi/js/three.min.js" type="text/javascript" ></script>
-        <script src="/assets/js/WebGL.js" type="text/javascript"> </script>
-        <script src="/assets/ldbi/js/OrbitControls.js" type="text/javascript" ></script>
-        <script src="/assets/ldbi/js/CopyShader.js" type="text/javascript" ></script>
-        <script src="/assets/ldbi/js/Pass.js" type="text/javascript" ></script>
-        <script src="/assets/ldbi/js/OutlinePass.js" type="text/javascript" ></script>
-        <script src="/assets/ldbi/js/ShaderPass.js" type="text/javascript" ></script>
-        <script src="/assets/ldbi/js/RenderPass.js" type="text/javascript" ></script>
-        <script src="/assets/ldbi/js/MaskPass.js" type="text/javascript" ></script>
-        <script src="/assets/ldbi/js/EffectComposer.js" type="text/javascript" ></script>
-        <script src="/assets/ldbi/js/colors.js" type="text/javascript" ></script>
-        <script src="/assets/js/ldraworgscene.js" type="text/javascript" ></script>    
-        <script src="/assets/ldbi/js/ClientStorage.js" type="text/javascript" ></script>
-        <script src="/assets/ldbi/js/LDROptions.js" type="text/javascript" ></script>
-        <script src="/assets/ldbi/js/LDRShaders.js" type="text/javascript" ></script>
-        <script src="/assets/ldbi/js/LDRColorMaterials.js" type="text/javascript" ></script>
-        <script src="/assets/ldbi/js/LDRGeometries.js" type="text/javascript" ></script>
-        <script src="/assets/ldbi/js/LDRBFCGeometries.js" type="text/javascript" ></script>
-        <script src="/assets/ldbi/js/LDRMeasurer.js" type="text/javascript" ></script>
-        <script src="/assets/ldbi/js/LDRLoader.js" type="text/javascript"></script>
-        <script src="/assets/ldbi/js/LDRGenerator.js" type="text/javascript"></script>
-        <script src="/assets/ldbi/js/LDRStuds.js" type="text/javascript"></script>
-        <script src="/assets/ldbi/js/VertexNormalsHelper.js"></script>
-        <script type="text/javascript">
-var part_paths;
-
-let canvas = document.getElementById('model-canvas');
-var scene;
-
-LDR.Options.bgColor = 0xFFFFFF;
-
-LDR.Colors.envMapPrefix = '/assets/ldbi/textures/cube/';    
-LDR.Colors.textureMaterialPrefix = '/assets/ldbi/textures/materials/';
-
-var idToUrl = function(id) {
-  if (part_paths[id]) {
-    return [part_paths[id]];
-  }
-  else {
-    return [id];
-  }
-};
-
-var idToTextureUrl = function(id) {
-  if (part_paths[id]) {
-    return part_paths[id];
-  }
-  else {
-    return id;
-  }
-};
-
-$( function() {
-    return false;
-  partID ='';
-  if ( !WEBGL.isWebGLAvailable() ) { return; }
-  if (!scene) {
-    // pre-fetch the paths to the subfiles used to speed up loading
-    var posting = $.get( '')
-      .done(function( response ) {
-        part_paths = response;
-        scene = new LDrawOrg.Model(canvas, partID, {idToUrl: idToUrl, idToTextureUrl: idToTextureUrl});
-        window.addEventListener('resize', () => scene.onChange(), false);
-    });
-  }
-
-});
-        </script>
-    @endpush
---}}    
 </x-layout.omr>
