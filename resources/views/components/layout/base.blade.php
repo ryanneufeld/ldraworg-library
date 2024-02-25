048d054c22f8fdb5e0c84495a70723127f49cbd5
Orion Pobursky <orion@ldraw.org>
2024-02-25 17:39:52 +0000

    Disable dashboad links

--- resources/views/components/layout/base.blade.php
+++ resources/views/components/layout/base.blade.php
@@ -54,11 +54,12 @@
             <div class="justify-self-end">
                 @auth
                     Welcome {{Auth::user()->name}} :: 
-                  
+{{--                  
                    <a href="{{route('filament.user.pages.dashboard')}}">User Dashboard</a>
                     @can('admin.view-dashboard')
                         :: <a href="{{route('filament.admin.pages.dashboard')}}">Admin Dashboard</a>
                     @endcan
+--}}
                 @endauth
             </div>
         </div>
