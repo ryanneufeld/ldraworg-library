8d5b73a765ec876a402c97ced640ddc0825ac85f
Orion Pobursky <orion@ldraw.org>
2024-03-01 04:38:57 +0000

    Remove extra text

--- resources/views/components/layout/base.blade.php
+++ resources/views/components/layout/base.blade.php
@@ -54,8 +54,8 @@
             </div>
             <div class="justify-self-end">
                 @auth
-                    Welcome {{Auth::user()->name}} :: 
-{{--                  
+                    Welcome {{Auth::user()->name}}{{-- :: 
+                  
                    <a href="{{route('filament.user.pages.dashboard')}}">User Dashboard</a>
                     @can('admin.view-dashboard')
                         :: <a href="{{route('filament.admin.pages.dashboard')}}">Admin Dashboard</a>
