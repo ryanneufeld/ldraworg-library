3891d49a3ff28b985ceb42c88566a9a6f195f595
Orion Pobursky <orion@ldraw.org>
2024-03-06 03:00:13 +0000

    Remove updated part text

--- resources/views/tables/columns/part-status.blade.php
+++ resources/views/tables/columns/part-status.blade.php
@@ -1,9 +1,5 @@
 <div>
     @if($getRecord()->isUnofficial())
         <x-part.status :part="$getRecord()" show-status />
-    @else
-        @isset ($getRecord()->unofficial_part_id)
-            <a href="{{ route('tracker.show', $getRecord()->unofficial_part_id) }}">Updated part on tracker</a>
-        @endisset
-    @endif    
+    @endif
 </div>
