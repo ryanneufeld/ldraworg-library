2b39ca2570445e54ddef69783695e7c1a1b50cea
Orion Pobursky <orion@ldraw.org>
2024-03-30 18:33:15 +0000

    Add daily digest option to test email

--- app/Console/Commands/SendEmail.php
+++ app/Console/Commands/SendEmail.php
@@ -2,7 +2,9 @@
 
 namespace App\Console\Commands;
 
+use App\Mail\DailyDigest;
 use App\Mail\TestEmail;
+use App\Models\PartEvent;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Mail;
 
@@ -13,7 +15,7 @@ class SendEmail extends Command
      *
      * @var string
      */
-    protected $signature = 'lib:test-email {user}';
+    protected $signature = 'lib:test-email {user} {--daily}';
 
     /**
      * The console command description.
@@ -29,6 +31,13 @@ class SendEmail extends Command
     {
         $rn = $this->argument('user');
         $user = \App\Models\User::firstWhere('name', $rn);
-        Mail::to($user)->send(new TestEmail(now(), 'This is a test message from the Parts Tracker'));
+        if ($this->option('daily')) {
+            $date = new \DateTime('yesterday');
+            $events = PartEvent::whereBetween('created_at', [$date, now()])
+                    ->whereIn('part_id', $user->notification_parts->pluck('id'))->oldest()->get();
+            Mail::to($user)->send(new DailyDigest($date, $events));
+        } else {
+            Mail::to($user)->send(new TestEmail(now(), 'This is a test message from the Parts Tracker'));
+        }
     }
 }
