2c51ce21c98f1f784d39974f8361fb4d2860b6a7
Orion Pobursky <orion@ldraw.org>
2024-04-23 02:58:46 +0000

    Turn status into anonymous componet

--- resources/views/components/part/status.blade.php
+++ resources/views/components/part/status.blade.php
@@ -1,3 +1,11 @@
+@props(['part', 'showStatus' => false])
 <div>
-    <x-fas-square class="inline w-5 fill-{{$color}}" /> {{$text}} {{$code}}
+    <x-fas-square @class([
+        'inline w-5',
+        'fill-lime-400' => $part->vote_sort == 1,
+        'fill-blue-700' => $part->vote_sort == 2,
+        'fill-gray-400' => $part->vote_sort == 3,
+        'fill-red-600' => $part->vote_sort == 5,
+
+    ])/> {{$showStatus ? $part->statusText() : ''}} {{$part->statusCode()}}
 </div>
