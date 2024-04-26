141696823ee0073b3b4d91923f0eaee994d8f472
Orion Pobursky <orion@ldraw.org>
2024-04-26 07:27:33 +0000

    Responsive table

--- app/Livewire/PartEvent/Index.php
+++ app/Livewire/PartEvent/Index.php
@@ -10,6 +10,8 @@ use Filament\Tables\Enums\FiltersLayout;
 use Filament\Tables\Filters\Filter;
 use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\Layout\Split;
+use Filament\Tables\Columns\Layout\Stack;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Columns\ViewColumn;
 use Filament\Tables\Concerns\InteractsWithTable;
@@ -41,37 +43,56 @@ class Index extends Component implements HasForms, HasTable
             ->query(PartEvent::query())
             ->defaultSort('created_at', 'desc')
             ->columns([
-                ViewColumn::make('part_event_type')
-                    ->view('components.event.icon.filament-table-icon')
-                    ->alignCenter()
-                    ->label('Event'),
-                TextColumn::make('user.name')
-                    ->description(fn (PartEvent $e): string => $e->user->realname ?? ''),
-                TextColumn::make('created_at')
-                    ->since()
-                    ->sortable()
-                    ->label('Date/Time'),
-                ImageColumn::make('image')
-                    ->state(
-                        function (PartEvent $event) {
-                            if (!is_null($event->part)) {
-                                return version("images/library/{$event->part->libFolder()}/" . substr($event->part->filename, 0, -4) . '_thumb.png');
-                            } else {
-                                return '';
+                Split::make([
+                    ViewColumn::make('part_event_type')
+                        ->view('components.event.icon.filament-table-icon')
+                        ->grow(false),
+                    TextColumn::make('created_at')
+                        ->since()
+                        ->sortable()
+                        ->label('Date/Time')
+                        ->grow(false),
+                    ImageColumn::make('image')
+                        ->state(
+                            function (PartEvent $event) {
+                                if (!is_null($event->part)) {
+                                    return version("images/library/{$event->part->libFolder()}/" . substr($event->part->filename, 0, -4) . '_thumb.png');
+                                } else {
+                                    return asset('images/library/placeholder.png');
+                                }
                             }
-                        }
-                    )
-                    ->extraImgAttributes(['class' => 'object-scale-down w-[35px] max-h-[75px]']),
-                TextColumn::make('part.filename')
-                    ->state(
-                        fn (PartEvent $e) =>
-                            !is_null($e->part) ? $e->part->filename : $e->deleted_filename
-                    )
-                    ->description(fn (PartEvent $e): string => !is_null($e->part) ? $e->part->description : $e->deleted_description)
-                    ->label('Part'),
-                ViewColumn::make('status')
-                    ->view('tables.columns.event-part-status')
-                    ->label('Status'),
+                        )
+                        ->extraImgAttributes(['class' => 'object-scale-down w-[35px] max-h-[75px]'])
+                        ->grow(false),
+                    TextColumn::make('user.name')
+                        ->description(fn (PartEvent $e): string => $e->user->realname ?? '')
+                        ->grow(false)
+                        ->visibleFrom('md'),
+                    TextColumn::make('part.filename')
+                        ->state(
+                            fn (PartEvent $e) =>
+                                !is_null($e->part) ? $e->part->filename : $e->deleted_filename
+                        )
+                        ->description(fn (PartEvent $e): string => !is_null($e->part) ? $e->part->description : $e->deleted_description)
+                        ->label('Part')
+                        ->visibleFrom('md'),
+                    Stack::make([
+                        TextColumn::make('user.name')
+                            ->description(fn (PartEvent $e): string => $e->user->realname ?? '')
+                            ->grow(false),
+                        TextColumn::make('part.filename')
+                            ->state(
+                                fn (PartEvent $e) =>
+                                    !is_null($e->part) ? $e->part->filename : $e->deleted_filename
+                            )
+                            ->description(fn (PartEvent $e): string => !is_null($e->part) ? $e->part->description : $e->deleted_description)
+                            ->label('Part'),
+                    ])->hiddenFrom('sm'),
+                    ViewColumn::make('status')
+                        ->view('tables.columns.event-part-status')
+                        ->label('Status')
+                        ->grow(false),
+                ])
             ])
             ->filters([
                 SelectFilter::make('part_event_type_id')
