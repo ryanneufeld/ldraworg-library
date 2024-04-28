cb3948a6759e18b4123e34e0f8d37df78e2e7e63
Orion Pobursky <orion@ldraw.org>
2024-04-28 08:39:39 +0000

    Added filters

--- app/Livewire/Tables/UserPartsTable.php
+++ app/Livewire/Tables/UserPartsTable.php
@@ -4,6 +4,8 @@ namespace App\Livewire\Tables;
 
 use App\Models\Part;
 use App\Tables\Part\PartTable;
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Tables\Filters\TernaryFilter;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\Auth;
@@ -23,6 +25,35 @@ class UserPartsTable extends BasicTable
             ->defaultSort('created_at', 'desc')
             ->heading('MySubmits')
             ->columns(PartTable::columns())
+            ->filters([
+                SelectFilter::make('vote_sort')
+                ->options([
+                    '1' => 'Certified',
+                    '2' => 'Needs Admin Review',
+                    '3' => 'Needs More Votes',
+                    '5' => 'Errors Found'
+                ])
+                ->native(false)
+                ->multiple()
+                ->preload()
+                ->label('Unofficial Status'),
+                SelectFilter::make('part_type_id')
+                    ->relationship('type', 'name')
+                    ->native(false)
+                    ->multiple()
+                    ->preload()
+                    ->label('Part Type'),
+                TernaryFilter::make('exclude_fixes')
+                    ->label('Fix Status')
+                    ->placeholder('All Parts')
+                    ->trueLabel('Exclude official part fixes')
+                    ->falseLabel('Only official part fixes')
+                    ->queries(
+                        true: fn (Builder $q) => $q->doesntHave('official_part'),
+                        false: fn (Builder $q) => $q->has('official_part'),
+                        blank: fn (Builder $q) => $q,
+                    ),
+            ])
             ->recordUrl(fn (Part $p): string => route('tracker.show', ['part' => $p]))
             ->queryStringIdentifier('userParts');
     }
