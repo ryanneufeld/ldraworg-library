0c1bf498fea50312ca013d0a64259c8370d867b3
Orion Pobursky <orion@ldraw.org>
2024-05-08 02:39:08 +0000

    Add render view manage

--- app/Livewire/PartRenderView/Manage.php
+++ app/Livewire/PartRenderView/Manage.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire\PartRenderView;
 
+use App\Models\Part;
 use App\Models\PartRenderView;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Concerns\InteractsWithForms;
@@ -34,12 +35,14 @@ class Manage extends Component implements HasForms, HasTable
             ])
             ->actions([
                 EditAction::make()
-                    ->form($this->formSchema()),
+                    ->form($this->formSchema())
+                    ->after(fn (PartRenderView $view) => $this->after($view)),
                 DeleteAction::make()
             ])
             ->headerActions([
                 CreateAction::make()
                     ->form($this->formSchema())
+                    ->after(fn (PartRenderView $view) => $this->after($view)),
             ]);
     }
 
@@ -47,14 +50,21 @@ class Manage extends Component implements HasForms, HasTable
     {
         return [
             TextInput::make('part_name')
-                ->description('Either a specific part (e.g. 973p01) or the base part name (973) for all parts of that name')
+                ->hint('Either a specific part (e.g. 973p01) or the base part name (973) for all parts of that name')
                 ->string()
                 ->required(),
             TextInput::make('matrix')
-                ->description('The rotation matrix to apply to the part render.')
+                ->hint('The rotation matrix to apply to the part render.')
                 ->string()
                 ->required(),
         ];
+    }
+
+    protected function after(PartRenderView $view): void
+    {
+        Part::where('filename', 'LIKE', "%{$view->part_name}%.dat")->each( fn (Part $p) =>
+            app(\App\LDraw\PartManager::class)->updatePartImage($p)
+        );
     }
 
     #[Layout('components.layout.admin')]
