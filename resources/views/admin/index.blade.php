72641f303f407929b6ac8ee29b0e5233beca529b
Orion Pobursky <orion@ldraw.org>
2024-05-11 06:02:33 +0000

    Obsolete files

--- resources/views/admin/index.blade.php
+++
@@ -1,7 +0,0 @@
-<x-layout.admin>
-    <x-slot name="title">Admin Dashboard</x-slot>
-    <h3 class="text-2xl font-bold">Admin Dashboard</h3>
-    <livewire:tables.part-ready-for-admin-table />
-    <livewire:tables.parts-flagged-for-deletion-table />
-    <livewire:tables.official-parts-with-errors-table />
-</x-layout.admin>
