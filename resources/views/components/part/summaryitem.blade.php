a9dbf3246ae51415415e4b40a91a3753388229d6
Orion Pobursky <orion@ldraw.org>
2024-05-26 18:55:28 +0000

    fix max height

--- resources/views/components/part/summaryitem.blade.php
+++ resources/views/components/part/summaryitem.blade.php
@@ -9,7 +9,7 @@
             <div class="bg-gray-200 font-bold p-2">
                 {{basename($part->filename, '.dat')}}
             </div>
-            <img class="p-2" src="{{version('images/library/' . $part->libFolder() . '/' . substr($part->filename, 0, -4) . '.png')}}" title="{{$part->description}}" alt="{{$part->description}}">
+            <img class="p-2 object-scale-down max-h-[150px]" src="{{version('images/library/' . $part->libFolder() . '/' . substr($part->filename, 0, -4) . '.png')}}" title="{{$part->description}}" alt="{{$part->description}}">
             <p class="text-sm p-2">{{$part->description}}</p>
             @if($part->isUnofficial())
                 <div class="p-2">
