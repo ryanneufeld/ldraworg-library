3a4f380aa618035431338d0eb8a7a563c340a333
Orion Pobursky <orion@ldraw.org>
2024-06-12 03:54:38 +0000

    filter using DB

--- app/LDraw/ScheduledTasks/SendDailyDigest.php
+++ app/LDraw/ScheduledTasks/SendDailyDigest.php
@@ -14,7 +14,7 @@ class SendDailyDigest {
     public function __invoke(): void {
         $users = User::whereHas('notification_parts', fn (Builder $q) =>
             $q->whereHas('events', fn (Builder $qu) => $qu->unofficial()->whereBetween('created_at', [Carbon::yesterday(), Carbon::today()]))
-        )->get();
+        )->where('is_legacy', false)->where('is_synthetic', false)->where('is_ptadmin', false)->get();
         foreach ($users as $user) {
             if ($user->is_legacy || $user->is_synthetic || $user->is_ptadmin) {
                 continue;
