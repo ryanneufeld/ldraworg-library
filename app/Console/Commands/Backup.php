d5c7b146aad203d5f73d4eccec3ae60fe24b762d
Orion Pobursky <orion@ldraw.org>
2024-08-25 23:40:02 +0000

    Update to use host and port, add db only option

--- app/Console/Commands/Backup.php
+++ app/Console/Commands/Backup.php
@@ -13,7 +13,7 @@ class Backup extends Command
      *
      * @var string
      */
-    protected $signature = 'lib:backup';
+    protected $signature = 'lib:backup {--db-only}';
 
     /**
      * The console command description.
@@ -35,53 +35,57 @@ class Backup extends Command
         $db = config('database.connections.mysql.database');
         $db_user = config('database.connections.mysql.username');
         $db_pw = config('database.connections.mysql.password');
-        $db_backup = Storage::disk('local')->path('backup/db_backup.sql');
+        $db_host = config('database.connections.mysql.host');
+        $db_port = config('database.connections.mysql.port');
 
-        $result = Process::forever()->run("mysqldump --user={$db_user} --password={$db_pw} $db > {$db_backup}");
+        $db_backup = Storage::disk('local')->path('backup/db_backup.sql');
+        $result = Process::forever()->run("mysqldump --user={$db_user} --password={$db_pw} --host={$db_host} --port={$db_port} $db > {$db_backup}");
 
         $this->info($result->errorOutput());
 
-        $zipname = Storage::disk('local')->path('backup/backup.zip');
+        if (!$this->option('db-only')) {
+            $zipname = Storage::disk('local')->path('backup/backup.zip');
 
-        $zip->open($zipname, \ZipArchive::CREATE | \ZipArchive::OVERWRITE);
-
-        $zip->addFile(base_path('.env'), '.env');
-        $zip->addFile($db_backup, 'storage/app/backup/db_backup.sql');
-
-        foreach(Storage::disk('local')->allFiles('assets') as $file) {
-            $zip->addFile(Storage::disk('local')->path($file), "storage/app/{$file}");
+            $zip->open($zipname, \ZipArchive::CREATE | \ZipArchive::OVERWRITE);
+    
+            $zip->addFile(base_path('.env'), '.env');
+            $zip->addFile($db_backup, 'storage/app/backup/db_backup.sql');
+    
+            foreach(Storage::disk('local')->allFiles('assets') as $file) {
+                $zip->addFile(Storage::disk('local')->path($file), "storage/app/{$file}");
+            }
+    
+            foreach(Storage::disk('local')->allFiles('deleted') as $file) {
+                $zip->addFile(Storage::disk('local')->path($file), "storage/app/{$file}");
+            }
+    
+            foreach(Storage::disk('images')->allFiles('banners') as $file) {
+                $zip->addFile(Storage::disk('images')->path($file), "storage/app/images/{$file}");
+            }
+            foreach(Storage::disk('images')->allFiles('cards') as $file) {
+                $zip->addFile(Storage::disk('images')->path($file), "storage/app/images/{$file}");
+            }
+            foreach(Storage::disk('images')->allFiles('updates') as $file) {
+                $zip->addFile(Storage::disk('images')->path($file), "storage/app/images/{$file}");
+            }
+            $zip->addFile(Storage::disk('images')->path('LDraw_Black_64x64.png'), 'storage/app/images/LDraw_Black_64x64.png');
+            $zip->addFile(Storage::disk('images')->path('LDraw_Green_64x64.png'), 'storage/app/images/LDraw_Green_64x64.png');
+            $zip->addFile(Storage::disk('images')->path('LDraw_Orange_64x64.png'), 'storage/app/images/LDraw_Orange_64x64.png');
+    
+            foreach(Storage::disk('library')->allFiles('official') as $file) {
+                $zip->addFile(Storage::disk('library')->path($file), "storage/app/library/{$file}");
+            }
+            $zip->close();
+            $zip->open($zipname);
+            foreach(Storage::disk('library')->allFiles('omr') as $file) {
+                $zip->addFile(Storage::disk('library')->path($file), "storage/app/library/{$file}");
+            }
+            $zip->close();
+            $zip->open($zipname);
+            foreach(Storage::disk('library')->allFiles('updates') as $file) {
+                $zip->addFile(Storage::disk('library')->path($file), "storage/app/library/{$file}");
+            }
+            $zip->close();                
         }
-
-        foreach(Storage::disk('local')->allFiles('deleted') as $file) {
-            $zip->addFile(Storage::disk('local')->path($file), "storage/app/{$file}");
-        }
-
-        foreach(Storage::disk('images')->allFiles('banners') as $file) {
-            $zip->addFile(Storage::disk('images')->path($file), "storage/app/images/{$file}");
-        }
-        foreach(Storage::disk('images')->allFiles('cards') as $file) {
-            $zip->addFile(Storage::disk('images')->path($file), "storage/app/images/{$file}");
-        }
-        foreach(Storage::disk('images')->allFiles('updates') as $file) {
-            $zip->addFile(Storage::disk('images')->path($file), "storage/app/images/{$file}");
-        }
-        $zip->addFile(Storage::disk('images')->path('LDraw_Black_64x64.png'), 'storage/app/images/LDraw_Black_64x64.png');
-        $zip->addFile(Storage::disk('images')->path('LDraw_Green_64x64.png'), 'storage/app/images/LDraw_Green_64x64.png');
-        $zip->addFile(Storage::disk('images')->path('LDraw_Orange_64x64.png'), 'storage/app/images/LDraw_Orange_64x64.png');
-
-        foreach(Storage::disk('library')->allFiles('official') as $file) {
-            $zip->addFile(Storage::disk('library')->path($file), "storage/app/library/{$file}");
-        }
-        $zip->close();
-        $zip->open($zipname);
-        foreach(Storage::disk('library')->allFiles('omr') as $file) {
-            $zip->addFile(Storage::disk('library')->path($file), "storage/app/library/{$file}");
-        }
-        $zip->close();
-        $zip->open($zipname);
-        foreach(Storage::disk('library')->allFiles('updates') as $file) {
-            $zip->addFile(Storage::disk('library')->path($file), "storage/app/library/{$file}");
-        }
-        $zip->close();
     }
 }
